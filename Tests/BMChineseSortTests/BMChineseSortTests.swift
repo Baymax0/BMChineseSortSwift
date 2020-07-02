bb6fbff39f2de15cf11f986019573b037b6ecf59
李志伟 <lee@leezw.local>
2020-07-02 15:23:33 +0800

    0.0.3
    0.0.3

--- Tests/BMChineseSortTests/BMChineseSortTests.swift
+++ Tests/BMChineseSortTests/BMChineseSortTests.swift
@@ -6,7 +6,7 @@ final class BMChineseSortTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(BMChineseSort().specialCharSectionTitle, "#")
+        XCTAssertEqual(BMChineseSort.share.specialCharSectionTitle, "#")
     }
 
     static var allTests = [
